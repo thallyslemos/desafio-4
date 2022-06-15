b0a754f09f826e46bdfe07ab6bc47021c357f745
Thallys <thallysvilemos@gmail.com>
2022-06-14 21:12:32 -0300

    form.js: Construtor da classe contato foi criado e será exibido no console o objeto quando a classe for chamada; contato.html: a função enviar será chamada após o acionamento do botao enviar e a id= nomeid foi inserida no campo nome

--- js/form.js
+++ js/form.js
@@ -1,8 +1,14 @@
-
 //class contato
-
 class contato {
-    
+    constructor(nome, sobrenome, email, cpf, telefone, contato){
+        this.nome = nome,
+        this.sobrenome = sobrenome,
+        this.email = email,
+        this.cpf = cpf,
+        this.telefone = telefone,
+        this.contato = contato;
+    }
+   
 }
 
 function Post(form) {
@@ -13,15 +19,15 @@ function Post(form) {
             form.elements.namedItem("cpf").value, 
             form.elements.namedItem("telefone").value, 
             form.elements.namedItem("contato").value);
-  
+
+  console.log(data);
 }
 
 function Enviar() {
-
     var nome = document.getElementById("nomeid");
+    console.log(nome);
 
-    if (nome.value != "") {
+    if (nome.value != "") {   
         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
     }
-
 }
